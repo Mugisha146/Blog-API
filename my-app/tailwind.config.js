501982dcbfad4c9dfc27183b537fcc95f800bc6c
Mugisha146 <emmyzizo1@gmail.com>
2024-08-14 23:38:49 +0200

    Blogs Project

---
+++ my-app/tailwind.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  content: ["./src/**/*.{js,jsx,ts,tsx}", "./public/index.html"],
+  theme: {
+    extend: {
+      colors: {
+        "teal-sorrow": "#your-teal-color-code",
+        primary: "#FBFCF8",
+        secondary: "#FFFFFF",
+      },
+    },
+  },
+  plugins: [],
+};
