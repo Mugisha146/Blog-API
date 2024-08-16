a33bd1b566b04cb40eb9a52f62b34362943235cc
Mugisha146 <emmyzizo1@gmail.com>
2024-08-16 15:02:56 +0200

    Fx Django Blog-API

--- my-app/src/components/Blog.tsx
+++ my-app/src/components/Blog.tsx
@@ -1,49 +1,52 @@
-
-
 import React, { useEffect, useState } from "react";
 import axios from "axios";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import "../styles/App.css";
 
 interface Post {
   id: number;
   title: string;
   author: string;
+  content: string; 
 }
 
 const Blog: React.FC = () => {
-  const [blogs, setblogs] = useState<Post[]>([]);
+  const [blogs, setBlogs] = useState<Post[]>([]);
+  const navigate = useNavigate();
 
   useEffect(() => {
     axios
       .get<Post[]>("http://127.0.0.1:8000/api/blogs/")
-      .then((response) => setblogs(response.data))
+      .then((response) => setBlogs(response.data))
       .catch((error) => console.error("Error fetching blogs:", error));
   }, []);
 
   const handleDelete = async (id: number) => {
     try {
       await axios.delete(`http://127.0.0.1:8000/api/blogs/${id}/`);
-      setblogs(blogs.filter((post) => post.id !== id));
+      setBlogs(blogs.filter((post) => post.id !== id));
     } catch (error) {
       console.error("Error deleting post:", error);
     }
   };
 
+  const handleEdit = (id: number) => {
+    navigate(`/edit/${id}`);
+  };
+
   return (
     <div className="bg-primary text-teal-sorrow">
-      <h1 className="text-3xl font-bold">blogs</h1>
+      <h1 className="text-3xl font-bold">Blogs</h1>
       <Link to="/new" className="text-blue-500">
         Create New Post
       </Link>
       <ul>
         {blogs.map((post) => (
           <li key={post.id} className="mt-4">
-            {post.title} by {post.author}
-            <Link
-              to={`http://127.0.0.1:8000/edit/${post.id}`}
-              className="text-blue-500 ml-2"
-            >
+            <h1 className="text-2xl font-bold">{post.title}</h1>
+            <p>{post.content}</p>
+            <p>Author: {post.author}</p>
+            <Link to={`/edit/${post.id}`} className="text-blue-500 ml-2">
               Edit
             </Link>
             <button
@@ -60,6 +63,3 @@ const Blog: React.FC = () => {
 };
 
 export default Blog;
-
-
-
